850377243a4985e346ccba0137a180cb345d38ee
Fairbanks, Ben-CW <Ben.Fairbanks@bestbuy.com>
2018-06-30 22:43:50 -0500

    Vertical menu - click title to show content horizontal menu - hover to show

--- functions.php
+++ functions.php
@@ -1,14 +1,34 @@
 <?php
-function my_theme_enqueue_styles() {
+    function surfx_squadrone_child_enqueue_styles() {
 
-    $parent_style = 'parent-style';
+        $parent_style = 'parent-style';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+        wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+        wp_enqueue_style( 'child-style',
+            get_stylesheet_directory_uri() . '/style.css',
+            array( $parent_style ),
+            wp_get_theme()->get('Version')
+        );
+    }
+
+    function surfx_squadrone_child_scripts() {
+        wp_enqueue_script(
+            'surfx_header',
+            get_stylesheet_directory_uri() . '/framework/js/header.misc.js',
+            array( 'jquery' ),
+            '',
+            true
+        );
+    }
+
+    wp_dequeue_script('squadrone-header');
+    
+    add_action( 'wp_enqueue_scripts', 'surfx_squadrone_child_scripts' );
+    add_action( 'wp_enqueue_scripts', 'surfx_squadrone_child_enqueue_styles' );
 ?>
+
+<!-- 
+    // commented out in parent functions.php
+    // custom theme js
+	// wp_enqueue_script( 'squadrone-header', get_template_directory_uri() . '/framework/js/header.misc.js', array( 'jquery' ), '', true );
+-->
